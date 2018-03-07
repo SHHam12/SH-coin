a3e4f80736f34110e25099aa962d19afe0f51e0d
SHHam12 <hamsukhwa@gmail.com>
2018-03-07 18:31:01 -0500

    Creating the block structure

---
+++ src/blockchain.js
@@ -0,0 +1,10 @@
+class Block {
+  constructor(index, hash, previousHash, timestamp, data){
+    this.index =index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.timestamp = timestamp;
+    this.data = data;
+  }
+}
+
