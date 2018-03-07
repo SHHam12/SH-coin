bcc73c8571e60cf5c7a0082f85079a736bba962b
SHHam12 <hamsukhwa@gmail.com>
2018-03-07 18:39:11 -0500

    Creating the Genesis Block

--- src/blockchain.js
+++ src/blockchain.js
@@ -8,3 +8,12 @@ class Block {
   }
 }
 
+const genesisBlock = new Block(
+  0,
+  "d7fe651764cd6b9a2488b8fa07ee8c80733f79068937e68fc0f9b8481361ad61",
+  null,
+  1520465646.297,
+  "This is the genesis~"
+);
+
+let blockchain = [genesisBlock];
