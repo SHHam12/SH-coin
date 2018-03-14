6d03101352b4d3deebdf0e026bf31d66c2905f8f
SHHam12 <hamsukhwa@gmail.com>
2018-03-14 00:06:13 -0400

    P2P Error Handling

--- src/p2p.js
+++ src/p2p.js
@@ -14,12 +14,22 @@ const startP2PServer = server => {
 
 const initSocketConnection = socket => {
   sockets.push(socket);
+  handleSocketError(socket);
   socket.on("message", (data) => {
     console.log(data);
   });
   setTimeout(() => {
      socket.send("welcome"); 
   }, 5000);
+};
+
+const handleSocketError = ws => {
+  const closeSocketConnection = ws => {
+    ws.close()
+    sockets.splice(sockets.indexOf(ws), 1);
+  };
+  ws.on("close", () => closeSocketConnection(ws));
+  ws.on("error", () => closeSocketConnection(ws));
 };
 
 const connectToPeers = newPeer => {
