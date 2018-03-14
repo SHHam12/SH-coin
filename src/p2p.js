e75b0c3239fa60f1564452c9608b6cdf8688c828
SHHam12 <hamsukhwa@gmail.com>
2018-03-14 16:47:53 -0400

    Sending Blocks Between Peers

--- src/p2p.js
+++ src/p2p.js
@@ -46,13 +46,34 @@ const initSocketConnection = ws => {
   sockets.push(ws);
   handleSocketMessages(ws);
   handleSocketError(ws);
+  sendMessage(ws, getLatest())
+};
+
+const parseData = data => {
+  try {
+    return JSON.parse(data)
+  } catch(e) {
+    console.log(e);
+    return null;
+  }
 };
 
 const handleSocketMessages = ws => {
   ws.on("message", data => {
-    
-  })
-}
+    const message = parseData(data);
+    if (message === null) {
+      return;
+    }
+    console.log(message);
+    switch (message.type) {
+      case GET_LATEST:
+        sendMessage(ws, getLastBlock());
+        break;
+    }
+  });
+};
+
+const sendMessage = (ws, message) => ws.send(JSON.stringify(message));
 
 const handleSocketError = ws => {
   const closeSocketConnection = ws => {
