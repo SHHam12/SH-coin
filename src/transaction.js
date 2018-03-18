9446ca184051d424d4ec9449bc5645f650637adf
SHHam12 <hamsukhwa@gmail.com>
2018-03-18 02:42:14 -0400

    Finished Validating Transaction Structure

--- src/transaction.js
+++ src/transaction.js
@@ -94,49 +94,55 @@ A(40) ---> TRANSACTION  ----> ZZ(10)
 */
 
 
-const isTxInStructureValid = (txIn) => {
-  if(txIn === null){
-    console.log("The txIn appears to be null")
+const isTxInStructureValid = txIn => {
+  if (txIn === null) {
+    console.log("The txIn appears to be null");
     return false;
-  } else if(typeof txIn.signature !== "string"){
-    console.log("The txIn doesn't have a valid signature")
+  } else if (typeof txIn.signature !== "string") {
+    console.log("The txIn doesn't have a valid signature");
     return false;
-  } else if(typeof txIn.txOutId !== "string"){
-    console.log("The txIn doesn't have a valid txOutId")
+  } else if (typeof txIn.txOutId !== "string") {
+    console.log("The txIn doesn't have a valid txOutId");
     return false;
-  } else if(typeof txIn.txOutIndex !== "number"){
-    console.log("The txIn doesn't have a valid txOutIndex")
+  } else if (typeof txIn.txOutIndex !== "number") {
+    console.log("The txIn doesn't have a valid txOutIndex");
     return false;
   } else {
     return true;
   }
-}
+};
 
-const isAddressValid = (address) => {
-  if(address.length !== 130){
+const isAddressValid = address => {
+  if (address.length !== 130) {
+    console.log("The address length is not the expected one");
     return false;
-  } else if(address.match("^[a-fA-F0-9]+$") === null){
-    return false
-  } else if(!address.startsWith("04")){
+  } else if (address.match("^[a-fA-F0-9]+$") === null) {
+    console.log("The address doesn't match the hex patter");
+    return false;
+  } else if (!address.startsWith("04")) {
+    console.log("The address doesn't start with 04");
     return false;
   } else {
     return true;
   }
-}
+};
 
-const isTxOutStructureValid = (txOut) => {
-  if(txOut === null){
+const isTxOutStructureValid = txOut => {
+  if (txOut === null) {
     return false;
-  } else if(typeof txOut.address !== "string"){
+  } else if (typeof txOut.address !== "string") {
+    console.log("The txOut doesn't have a valid string as address");
     return false;
-  } else if(!isAddressValid(txOut.address)){
+  } else if (!isAddressValid(txOut.address)) {
+    console.log("The txOut doesn't have a valid address");
     return false;
-  } else if(typeof txOut.amount !== "number"){
+  } else if (typeof txOut.amount !== "number") {
+    console.log("The txOut doesn't have a valid amount");
     return false;
   } else {
     return true;
   }
-}
+};
 
 const isTxStructureValid = tx => {
   if (typeof tx.id !== "string") {
@@ -145,16 +151,20 @@ const isTxStructureValid = tx => {
   } else if (!(tx.txIns instanceof Array)) {
     console.log("The txIns are not an array");
     return false;
-  } else if(){
-    console.log("The structure of one of the txIn is not valid")
+  } else if (
+    !tx.txIns.map(isTxInStructureValid).reduce((a, b) => a && b, true)
+  ) {
+    console.log("The structure of one of the txIn is not valid");
     return false;
-  } else if(!(tx.txOuts instanceof Array)){
-    console.log("The txOuts are not an array")
+  } else if (!(tx.txOuts instanceof Array)) {
+    console.log("The txOuts are not an array");
     return false;
-  } else if(){
-    console.log("The structure of one of the txOut is not valid")
+  } else if (
+    !tx.txOuts.map(isTxOutStructureValid).reduce((a, b) => a && b, true)
+  ) {
+    console.log("The structure of one of the txOut is not valid");
     return false;
   } else {
-    return true
+    return true;
   }
 };
