038cd3c456307e342d26995bce8375364bc87435
SHHam12 <hamsukhwa@gmail.com>
2018-03-17 20:38:56 -0400

    define transaction class

---
+++ src/transaction.js
@@ -0,0 +1,29 @@
+class TxOut { // transaction output
+  constructor(address, amount) {
+    this.address = address;
+    this.amount = amount;
+  }
+}
+
+class TxIn { // transaction input
+  // uTxOutId
+  // uTxOutIndex
+  // Signature
+}
+
+class Transaction {
+  // ID
+  // txIns[]
+  // txOuts[]
+}
+
+class UTxOut { // unspend transaction output
+  constructor(uTxOutId, uTxOutIndex, address, amount) {
+    this.uTxOutId = uTxOutId;
+    this.uTxOutIndex = uTxOutIndex;
+    this.address = address;
+    this.amount = amount;
+  }
+}
+
+let uTxOuts = [];
