2949a4cb46c23b136e4ed85df6f994412cc69481
SHHam12 <hamsukhwa@gmail.com>
2018-03-20 10:16:40 -0400

    Creating a Coinbase Transaction

--- src/transactions.js
+++ src/transactions.js
@@ -263,11 +263,24 @@ const validateCoinbaseTx = (tx, blockIndex) => {
   }
 };
 
+const createCoinbaseTx = (address, blockIndex) => {
+  const tx = new Transaction();
+  const txIn = new TxIn();
+  txIn.signature = "";
+  txIn.txOutId = blockIndex;
+  tx.txIns[txIn];
+  tx.txOut = [new TxOut(address, COINBASE_AMOUNT)];
+  tx.id = getTxId(tx);
+  return tx;
+};
+
+
 module.exports = {
   getPublicKey,
   getTxId,
   signTxIn,
   TxIn,
   Transaction,
-  TxOut
+  TxOut,
+  createCoinbaseTx
 };
