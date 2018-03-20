3ec373ff24e9d434c02705106ac681c8421ec37f
SHHam12 <hamsukhwa@gmail.com>
2018-03-19 21:57:30 -0400

    Getting Address and Address Balance

--- src/wallet.js
+++ src/wallet.js
@@ -1,6 +1,7 @@
 const elliptic = require("elliptic"),
   path = require("path"),
   fs = require("fs");
+  _ = require("lodash");
 
 const ec = new elliptic.ec("secp256k1");
 
@@ -11,6 +12,24 @@ const generatePrivateKey = () => {
   const privateKey = keyPair.getPrivate();
   return privateKey.toString(16);
 };
+
+const getPrivateFromWallet = () => {
+  const buffer = fs.readFileSync(privateKeyLoaction, "utf-8");
+  buffer.toString();
+};
+
+const getPublicFromWallet = () => {
+  const privateKey = getPrivateFromWallet();
+  const key = ec.keyFromPrivate(privateKey, "hex");
+  return key.getPublic().encode("hex");
+};
+
+const getBalance = (address, uTxOuts) => {
+  return _(uTxOuts)
+    .filter(uTxO => uTxO.address === address)
+    .map(uTxO => uTxO => amount)
+    .sum();
+}
 
 const initWallet = () => {
   if (fs.existsSync(privateKeyLoaction)) {
