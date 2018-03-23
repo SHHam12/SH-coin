75cf36679d60822da08aa13acc7d92ce562a59b0
SHHam12 <hamsukhwa@gmail.com>
2018-03-23 02:23:21 -0400

    API to get address

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const express = require("express"),
 
 const { getBlockchain, createNewBlock, getAccountBalance, sendTx } = Blockchain;
 const { startP2PServer, connectToPeers } = P2P;
-const { initWallet } = Wallet;
+const { initWallet, getPublicFromWallet } = Wallet;
 const { getMempool } = Mempool;
 
 const PORT = process.env.HTTP_PORT || 4000;
@@ -36,6 +36,10 @@ app.post("/peers", (req, res) => {
 app.get("/me/balance", (req, res) => {
   const balance = getAccountBalance();
   res.send({ balance });
+});
+
+app.get("/me/address", (req, res) => {
+  res.send(getPublicFromWallet());
 });
 
 app
